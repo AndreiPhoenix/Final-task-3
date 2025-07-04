ce3210480b1ba0be0f6eb5629d46b9906bc979ff
AndreiPhoenix <145689886+AndreiPhoenix@users.noreply.github.com>
2025-07-04 20:17:49 +0500

    index.tsx

---
+++ index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// Ionic CSS
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import './theme/variables.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
