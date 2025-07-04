58f7bc5827e3bbcb03cec47c7a9b9f0cf22f3c17
AndreiPhoenix <145689886+AndreiPhoenix@users.noreply.github.com>
2025-07-04 20:00:28 +0500

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// Ionic styles
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import './theme/variables.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
